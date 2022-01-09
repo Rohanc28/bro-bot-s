33b50f1a8ff82a26e6ab48773cac4a265abc257b
Rohanc28 <81807980+Rohanc28@users.noreply.github.com>
2022-01-09 20:37:56 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,61 @@
+
+# BroBOT - README
+Made by Rohan Chaturvedi 
+
+## Invite [Link](https://discord.com/api/oauth2/authorize?client_id=821771855400665181&permissions=0&scope=bot) 
+
+## Commands:
+Bro BOT looks for prefix "bro" to detect messages. Some commands it responds to.
+```
+on?
+bot ip
+bro help
+bro roll
+bro comic
+bro joke
+bro trivia
+bro coin
+covid
+github
+sup/Sup
+```
+## Type "bro help" to know all current commands the bot replies to
+```
+bro help
+```
+![image](https://user-images.githubusercontent.com/81807980/137751799-7bb74443-1a31-42ce-8c76-0ce3d2bdc1d6.png)
+
+## Screenshots:
+
+on?.
+
+![image](https://user-images.githubusercontent.com/81807980/129919086-73a8e91b-8b72-4e18-977a-13edce299b48.png)
+
+bro roll (to roll dice).
+
+![image](https://user-images.githubusercontent.com/81807980/129919166-fcedd7c9-5b13-4dd7-848e-f38a28ec0dee.png)
+
+bro comic (for XKCD comics).
+
+![image](https://user-images.githubusercontent.com/81807980/138320271-4a279b75-1f3b-4533-928f-455c8f60c087.png)
+
+bro joke (for two liner jokes).
+
+![image](https://user-images.githubusercontent.com/81807980/129919353-321a9a8c-1651-4a3a-8400-2cc76efa132f.png)
+
+bro coin (for bitcoin value).
+
+![image](https://user-images.githubusercontent.com/81807980/129920198-8d82da2f-7777-4acb-b381-2275227cc238.png)
+
+covid (for Covid-19 India Statistics from Apify Covid API).
+
+![image](https://user-images.githubusercontent.com/81807980/129919594-75316fb8-ccb4-450b-a688-cd266e8b71dd.png)
+
+bro trivia
+
+![image](https://user-images.githubusercontent.com/81807980/138320712-9de34ba9-538a-48f3-a012-fb805eb4eb0c.png)
+
+
+
+#Acknowledgements:
+Thanks to Rapptz (discord.py), Jonah Lawrence (help with deploying on Heroku), and several StackoverFlow users for helping me solve minor bugs.
---
+++ discord_bot_testing_script.py
@@ -0,0 +1,100 @@
+import time
+import pyautogui
+i=1
+while (i>0):
+    print("Starting...")
+    time.sleep(5) 
+    pyautogui.write('on?')  
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+    
+    pyautogui.write('bot invite')  
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+    
+    pyautogui.write('bot ip') 
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+    
+    pyautogui.write('bro help') 
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+    
+    pyautogui.write('bro roll') 
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+    
+    pyautogui.write('bro comic ')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+    
+    pyautogui.write('bro joke ')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+    
+    pyautogui.write('bro trivia') 
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5)
+    
+    pyautogui.write('bro coin') 
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+    
+    pyautogui.write('covid') 
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+    
+    pyautogui.write('github')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+    
+    pyautogui.write('bro njoke')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+    
+    pyautogui.write('Hi')  
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+    
+    pyautogui.write('hi')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+    
+    pyautogui.write('sup')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+    
+    pyautogui.write('Sup')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    
+    pyautogui.write("**100% script complete**")
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(5) 
+
+    print("Script end")
+    i-=1
+    
+#loading--
+
+#|█----| 20%
+#|██---| 40%
+#|███--| 60%
+#|████-| 80%
+#|█████| 100%
