f593211c8622284a1f03a7da8a2d3a9e891d31d3
Rohanc28 <rochaturvedi01@gmail.com>
2022-02-13 22:06:21 +0530

    Update discord_bot_testing_script.py

--- discord_bot_testing_script.py
+++ discord_bot_testing_script.py
@@ -1,92 +1,106 @@
 import time
 import pyautogui
 i=1
+a=int(3)
 while (i>0):
     print("Starting...")
-    time.sleep(5) 
+    time.sleep(4) 
+    #|██████| 100% Script
+    pyautogui.write('|█-----| 100% Starting')  
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(a)
     pyautogui.write('on?')  
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    time.sleep(a) 
     
     pyautogui.write('bot invite')  
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    time.sleep(a) 
     
     pyautogui.write('bot ip') 
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    time.sleep(a) 
     
     pyautogui.write('bro help') 
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    time.sleep(a) 
     
     pyautogui.write('bro roll') 
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    time.sleep(a) 
     
     pyautogui.write('bro comic ')
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    time.sleep(a) 
     
     pyautogui.write('bro joke ')
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    time.sleep(a) 
     
     pyautogui.write('bro trivia') 
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5)
+    time.sleep(a)
     
     pyautogui.write('bro coin') 
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    time.sleep(a) 
     
     pyautogui.write('covid') 
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    time.sleep(a) 
     
     pyautogui.write('github')
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    time.sleep(a) 
     
     pyautogui.write('bro njoke')
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    time.sleep(a) 
     
     pyautogui.write('Hi')  
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    time.sleep(a) 
     
     pyautogui.write('hi')
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    time.sleep(a) 
     
     pyautogui.write('sup')
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    time.sleep(a) 
     
     pyautogui.write('Sup')
     time.sleep(0.1)
     pyautogui.press('enter')
     
-    pyautogui.write("**100% script complete**")
+    pyautogui.write('bro guess')
     time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(5) 
+    
+    pyautogui.write('a')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    
+    pyautogui.write("** :logow1: 100% Script complete**")
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(a) 
 
     print("Script end")
     i-=1
